9ec48c9bad5c7f5b72c97d426849e8f59331edee
devakhilus <devakhilus@gmail.com>
2025-03-03 18:44:15 +0530

    datatype

---
+++ php-sandbox/2.datatype.php
@@ -0,0 +1,38 @@
+<?php
+
+//strings
+$string1 = 'hai this is a string';
+$string2 = 'this also a string';
+var_dump($string1);
+echo '<br/>';
+var_dump($string2);
+echo '<br/>';
+//integer
+$subMark = 200;
+var_dump($subMark);
+echo '<br/>';
+//float
+$floatMark = 2.5;
+var_dump($floatMark);
+echo '<br/>';
+//array
+$arrayMark = ['204', 'Maths'];
+var_dump($arrayMark);
+echo '<br/>';
+//objects
+
+$objectsM = new stdClass;
+var_dump($objectsM);
+
+
+//boolean
+$ageGn = true;
+var_dump($ageGn);
+
+//null
+
+$pcat = null;
+var_dump(null);
+
+//resource
+// $file = fopen('tex.ini', 'r');
