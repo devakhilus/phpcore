60c9d43a0ec38fed6ec100bd74378c5de77b3d67
devakhilus <devakhilus@gmail.com>
2025-03-04 14:17:44 +0530

    Intro To Arrays

---
+++ php-sandbox/9.Intro To Arrays.php
@@ -0,0 +1,39 @@
+<?php
+// $names = array('amal', 'akhil');
+$names = ['amal', 'akhil'];
+echo $names[1];
+$numbers = [2, 4, 6, 8, 10];
+/*echo '<pre>';
+var_dump($names);
+echo '<pre>';
+die; */
+function dispaly($values)
+{
+    echo '<pre>';
+    var_dump($values);
+    echo '<pre>';
+}
+dispaly($names);
+dispaly($numbers);
+print_r($names);
+
+echo $names[1];
+echo $numbers[2];
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <p>hai</p>
+</body>
+
+</html>
